6b8f3618319db8c3344ad4ff2aec3daf6dfbdd22
Roma Kokontsev <Romeo42@ukr.net>
2021-07-31 19:40:22 +0300

    add hooks

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,46 +1,79 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
 import PropTypes from "prop-types";
 
 import s from "./Modal.module.css";
 
-class Modal extends Component {
-  static propTypes = {
-    onBackdrop: PropTypes.func.isRequired,
-    content: PropTypes.string.isRequired,
-  };
+export default function Modal({ content, onBackdrop }) {
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
 
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
-
-  handleKeyDown = (e) => {
+  const handleKeyDown = (e) => {
     if (e.code === "Escape") {
-      this.props.onBackdrop();
+      onBackdrop();
     }
   };
 
-  handleBackdropClick = (e) => {
-    if (e.target === e.currentTarget) {
-      this.props.onBackdrop();
+  const handleBackdropClick = (event) => {
+    if (event.currentTarget === event.target) {
+      onBackdrop();
     }
   };
 
-  render() {
-    const { content } = this.props;
-    console.log(content);
-    return (
-      <div className={s.Overlay} onClick={this.handleBackdropClick}>
-        <div className={s.Modal}>
-          <img src={content} alt="" />
-        </div>
+  return (
+    <div className={s.Overlay} onClick={handleBackdropClick}>
+      <div className={s.Modal}>
+        <img src={content} alt="" />
       </div>
-    );
-  }
+    </div>
+  );
 }
+Modal.propTypes = {
+  onBackdrop: PropTypes.func.isRequired,
+  content: PropTypes.string.isRequired,
+};
 
-export default Modal;
+// class Modal extends Component {
+// static propTypes = {
+//   onBackdrop: PropTypes.func.isRequired,
+//   content: PropTypes.string.isRequired,
+// };
+
+//   componentDidMount() {
+//     window.addEventListener("keydown", this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener("keydown", this.handleKeyDown);
+//   }
+
+//   handleKeyDown = (e) => {
+//     if (e.code === "Escape") {
+//       this.props.onBackdrop();
+//     }
+//   };
+
+//   handleBackdropClick = (e) => {
+//     if (e.target === e.currentTarget) {
+//       this.props.onBackdrop();
+//     }
+//   };
+
+// render() {
+//   const { content } = this.props;
+//   console.log(content);
+//   return (
+//     <div className={s.Overlay} onClick={this.handleBackdropClick}>
+//       <div className={s.Modal}>
+//         <img src={content} alt="" />
+//       </div>
+//     </div>
+//   );
+// }
+// }
+
+// export default Modal;
